51240544c0ab5cd1e0f7bacf3bb429e2ca6e94eb
Diana Utan <didiutzi@gmail.com>
2021-04-23 14:54:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution here
 function findMatching(drivers, name) {
+<<<<<<< HEAD
   return drivers.filter(driverName => driverName.toLowerCase() === name.toLowerCase())
 }
 
@@ -9,5 +10,16 @@ function fuzzyMatch(drivers, name) {
 }
 
 function matchName(drivers, name) {
+=======
+  return arr.filter(driverName => driverName.toLowerCase() === name.toLowerCase())
+}
+
+function findMatching(drivers, name) {
+  let nameLength = mane.length;
+  return drivers.filter(driverName => driverName.slice(0, nameLength) === name)
+}
+
+function findMatching(drivers, name) {
+>>>>>>> 4e9c746c7fdd0741167541431a1fc30962511788
   return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase())
 }
