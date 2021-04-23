5297dfa4ffe67c29f0789c7be18bce00945fb3e6
Diana Utan <didiutzi@gmail.com>
2021-04-23 02:54:28 +0000

    used filter to comlete findMatching, fuzzyMatching and matchName functions

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(driverName => driverName.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, name) {
+  let nameLength = name.length;
+  return drivers.filter(driverName => driverName.slice(0, nameLength) === name)
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase())
+}
